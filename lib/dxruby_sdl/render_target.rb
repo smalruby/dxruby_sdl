3ee6ad6849043bdf1da128f77ecc562f06e6f597
Kouji Takao <kouji.takao@gmail.com>
2017-09-23 23:24:47 +0900

    added RenderTarget#to_image, #draw_ex.

--- lib/dxruby_sdl/render_target.rb
+++ lib/dxruby_sdl/render_target.rb
@@ -6,20 +6,62 @@ module DXRubySDL
   class RenderTarget
     extend Forwardable
 
-    attr_reader :_surface
-
     def initialize(width, height, color)
-      @image = Image.new(width, height, color)
-      @_surface = @image._surface
+      @_image = Image.new(width, height, color)
+      @disposed = false
     end
 
-    def_delegators :@image, :draw
-    
+    def dispose
+      @disposed = true
+      @_image = nil
+    end
+
+    def disposed?
+      @disposed
+    end
+
+    def draw(*args)
+      check_disposed
+      @_image.draw(*args)
+    end
+
+    def draw_ex(x, y, image, hash = {})
+      check_disposed
+      if hash[:z] && hash[:z] != 0
+        raise NotImplementedError, 'Window.draw_ex(x, y, image, z: != 0)'
+      end
+      option = {
+        angle: 0,
+        scale_x: 1,
+        scale_y: 1,
+        center_x: 0,
+        center_y: 0,
+      }.merge(hash)
+      SDL::Surface.transform_blit(image._surface, @_image._surface,
+        option[:angle],
+        option[:scale_x], option[:scale_y],
+        option[:center_x], option[:center_y],
+        x + option[:center_x], y + option[:center_y],
+        0)
+    end
+
     def draw_font(x, y, string, font, option = {})
+      check_disposed
       color = option[:color] || [255, 255, 255]
-      @image.draw_font(x, y, string, font, color) 
+      @_image.draw_font(x, y, string, font, color)
     end
 
-    alias_method :drawFont, :draw_font
+    def to_image
+      check_disposed
+      @_image
+    end
+
+    private
+
+    def check_disposed
+      if disposed?
+        raise
+      end
+    end
   end
 end
